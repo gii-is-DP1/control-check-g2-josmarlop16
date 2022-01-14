0854d6443917443fd3b2ced4f209b7d33db16ca4
josmarlop16 <joseantonio@localhost>
2022-01-14 11:52:18 +0100

    entrega josmarlop16

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,5 +1,51 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.PetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/feeding")
 public class FeedingController {
+
+    @Autowired FeedingService feedingService;
+    @Autowired PetService petService;
+
+    private static final String VIEWS_FEEDING_CREATE_OR_UPDATE_FORM = "feedings/createOrUpdateFeedingForm";
+
+    @GetMapping(path = "/create")
+    public String initCreationForm(ModelMap modelMap) {
+        String view = VIEWS_FEEDING_CREATE_OR_UPDATE_FORM;
+        modelMap.addAttribute("feeding", new Feeding());
+        modelMap.addAttribute("feedingTypes", feedingService.getAllFeedingTypes());
+        modelMap.addAttribute("pets", petService.getAllPets());
+        return view;
+    }
+
+
+    //LO INTENTE PERO NO TENIA CLARO COMO CONTINUAR...
+    /*
+    @PostMapping(path = "create")
+    public String processCreationForm(@Valid Feeding feeding, BindingResult result, ModelMap modelMap) throws UnfeasibleFeedingException {
+        String view = "welcome";
+        if (result.hasErrors()) {
+            modelMap.addAttribute("feeding", feeding);
+            modelMap.addAttribute("feedingType", feedingService.getAllFeedingTypes());
+            return VIEWS_FEEDING_CREATE_OR_UPDATE_FORM;
+        } else {
+            feedingService.save(feeding);
+            modelMap.addAttribute("message", "Feeding succesfully saved!");
+        }
+        return view;
+    }
+    */
+    
     
 }
